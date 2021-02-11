5cdb7b7b6ca6fff9d916e1399eec6ee92ba69b34
Fitsum Sileshi <fitsesile@gmail.com>
2021-02-11 11:33:42 +0300

    test img

--- examplr.py
+++ examplr.py
@@ -2,8 +2,9 @@ import cv2
 
 trained_face_data=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img= cv2.imread("RDJ.png")
+img= cv2.imread("RD.jpg")
 
-
+cv2.imshow("Face Detector", img)
+cv2.waitKey()
 
 print("code compiled")
