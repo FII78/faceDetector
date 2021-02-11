c69d0452394b2a6356c495d56bf947337a1790c6
Fitsum Sileshi <fitsesile@gmail.com>
2021-02-11 11:38:13 +0300

    converted color

--- examplr.py
+++ examplr.py
@@ -3,8 +3,8 @@ import cv2
 trained_face_data=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 img= cv2.imread("RD.jpg")
-
-cv2.imshow("Face Detector", img)
+greyScaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("Face Detector", greyScaled_img)
 cv2.waitKey()
 
 print("code compiled")
