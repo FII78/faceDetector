ba0c315294b62e7547ede74ace1962fe6827144b
Fitsum Sileshi <fitsesile@gmail.com>
2021-02-11 12:29:06 +0300

    webcam capture

--- examplr.py
+++ examplr.py
@@ -2,7 +2,10 @@ import cv2
 
 trained_face_data=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img= cv2.imread("RD.jpg")
+# img= cv2.imread("RD.jpg")
+webcam=cv2.VideoCapture(0) #0 means from webcam by default
+
+
 greyScaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 face_coordinates=trained_face_data.detectMultiScale(greyScaled_img)
