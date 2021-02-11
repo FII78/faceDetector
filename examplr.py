d32b7c2304a7e8908235ffcea1af6fbc4ee9f377
Fitsum Sileshi <fitsesile@gmail.com>
2021-02-11 11:52:17 +0300

    rectanlge coordinate

--- examplr.py
+++ examplr.py
@@ -4,6 +4,10 @@ trained_face_data=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 img= cv2.imread("RD.jpg")
 greyScaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+face_coordinates=trained_face_data.detectMultiScale(greyScaled_img)
+print(face_coordinates)
+
 cv2.imshow("Face Detector", greyScaled_img)
 cv2.waitKey()
 
