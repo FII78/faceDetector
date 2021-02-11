159324ae5ae3294c7baea10d4a6d5bf33b1ce095
Fitsum Sileshi <fitsesile@gmail.com>
2021-02-11 12:03:29 +0300

    test detenction

--- examplr.py
+++ examplr.py
@@ -8,7 +8,9 @@ greyScaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 face_coordinates=trained_face_data.detectMultiScale(greyScaled_img)
 print(face_coordinates)
 
-cv2.imshow("Face Detector", greyScaled_img)
+cv2.rectangle(img,(77,98),(233+77,233+98),(0,255,0),2)
+
+cv2.imshow("Face Detector", img)
 cv2.waitKey()
 
 print("code compiled")
