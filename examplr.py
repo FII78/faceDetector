38c57787fb25323a227c7df83bbef23a9e98eb28
Fitsum Sileshi <fitsesile@gmail.com>
2021-02-11 14:22:56 +0300

    after break

--- examplr.py
+++ examplr.py
@@ -4,20 +4,23 @@ trained_face_data=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # img= cv2.imread("RD.jpg")
 webcam=cv2.VideoCapture(0) #0 means from webcam by default
+key=cv2.waitKey(1)
+
+while True:
+    successful_frame_read,frame=webcam.read()
+
+    greyScaled_img=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    # face_coordinates=trained_face_data.detectMultiScale(greyScaled_img)
+    # print(face_coordinates)
+
+    # cv2.rectangle(img,(77,98),(233+77,233+98),(0,255,0),2)
+
+    for (x,y,w,h) in face_coordinates:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
 
 
-greyScaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Face Detector", img)
+    cv2.waitKey()
 
-face_coordinates=trained_face_data.detectMultiScale(greyScaled_img)
-print(face_coordinates)
-
-# cv2.rectangle(img,(77,98),(233+77,233+98),(0,255,0),2)
-
-for (x,y,w,h) in face_coordinates:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
-
-
-cv2.imshow("Face Detector", img)
-cv2.waitKey()
-
-print("code compiled")
+    print("code compiled")
